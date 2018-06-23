224a67e303a78e34fc39ed2c0dbe92f390644676
Srija S <srijakumar7@gmail.com>
2018-06-23 20:08:38 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,5 @@
-CREATE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, end_date INTEGER);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, end_date INTEGER);
 
-CREATE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
 
-CREATE pledges (id INTEGER PRIMARY KEY, user TEXT, project TEXT);
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, user TEXT, project TEXT);
